665cfc4f9d287e8c1da22616fcacbd862ecd025e
JoseCarlosOlmedilha <jose.olmedilha@outlook.com>
2025-07-11 00:23:02 -0300

    Implementasndo logica de negocio para a classe de clientes no service

--- app/app/src/main/java/br/com/loja/app/service/ClienteService.java
+++ app/app/src/main/java/br/com/loja/app/service/ClienteService.java
@@ -1,9 +1,13 @@
 package br.com.loja.app.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import br.com.loja.app.entity.Cliente;
+import br.com.loja.app.entity.Endereco;
+import br.com.loja.app.entity.Telefone;
 import br.com.loja.app.repository.ClienteRepository;
 
 @Service
@@ -14,10 +18,46 @@ private ClienteRepository clienteRepository;
 
 
 public Cliente cadastrarCliente(Cliente cliente){
-
-    Cliente clienteSalvo = clienteRepository.save(cliente);
-
-    return clienteSalvo;
+    return null;
 }
 
+public Cliente excluirCliente(Cliente cliente){
+    return null;
 }
+
+public Cliente buscarCliente(Cliente cliente){
+    return null;
+
+}
+
+private Boolean verificarTelefones(List<Telefone> telefones){
+    return null;
+}
+
+private Boolean verificarEndereco(Endereco endereco){
+    if(endereco == null){
+        throw new IllegalArgumentException("Endereço não pode ser nulo");
+    }
+
+    return true;
+}
+
+private Boolean verificarCpf(Cliente cliente){
+    return null;
+}
+
+private Boolean verificaSexo(Cliente cliente){
+    Character sexo = cliente.getSexo();
+
+    if(sexo != 'M' && sexo != 'F' ){
+        throw new IllegalArgumentException("Sexo inválido, o valor de sexo tem que ser M ou F");
+    }
+
+    return true;
+
+}
+
+
+
+}
+
